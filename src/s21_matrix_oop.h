f5fbb6c705d5a9f34bbae406f0e06f6aef146ccd
Zulma Alan <zulmaala@at-f3.msk.21-school.ru>
2023-06-16 19:58:06 +0300

    first commit

---
+++ src/s21_matrix_oop.h
@@ -0,0 +1,45 @@
+#ifndef S21_MATRIX_OPP_H_
+#define S21_MATRIX_OPP_H_
+
+#include <iostream>
+#include <cmath>
+#define E 1e-7
+using namespace std;
+
+class S21Matrix {
+    public:
+        S21Matrix();              // Default constructor
+        ~S21Matrix();             // Destructor
+        S21Matrix(int rows, int cols);
+        S21Matrix(S21Matrix&& other);
+        S21Matrix(const S21Matrix& other);
+
+        //Operators
+        S21Matrix operator+(const S21Matrix& other);
+        S21Matrix operator-(const S21Matrix& other);
+
+        //Assignement operators
+        S21Matrix& operator+=(const S21Matrix& other);
+        S21Matrix& operator-=(const S21Matrix& other);
+
+        //getters and setters
+        void set_rows(int rows);
+        void set_cols(int cols);
+        int get_rows();
+        int get_cols();
+
+        //Basics' operations
+        void SumMatrix(const S21Matrix& other); 
+        void SubMatrix(const S21Matrix& other);
+
+        // Other methods..
+    private:
+        // Attributes
+        int rows_, cols_;         // Rows and columns
+        double **matrix_;         // Pointer to the memory where the matrix is allocated
+        void mem_alloc();
+        void destroy_matrix();
+        void copy_matrix(const S21Matrix& other);
+};
+
+#endif
---
+++ src/test_matrix_oop.cpp
@@ -0,0 +1,81 @@
+#include <gtest/gtest.h>
+#include "s21_matrix_oop.h"
+
+TEST(SumMatrix, True) {
+    S21Matrix matrix_a(2, 2);
+    S21Matrix matrix_b(2, 2);
+    S21Matrix result(2, 2);
+
+    matrix_a(0, 0) = 3.14;
+    matrix_a(0, 1) = 0.56;
+    matrix_a(1, 0) = -69.3;
+    matrix_a(1, 1) = 0;
+
+    matrix_b(0, 0) = -78.14;
+    matrix_b(0, 1) = 0;
+    matrix_b(1, 0) = -0.3;
+    matrix_b(1, 1) = 2;
+
+    result(0, 0) = -75;
+    result(0, 1) = 0.56;
+    result(1, 0) = -69.6;
+    result(1, 1) = 2;
+    matrix_a.SumMatrix(matrix_b);
+    ASSERT_TRUE(matrix_a == result);
+}
+TEST(SumMatrix, False) {
+    S21Matrix matrix_a(1, 2);
+    S21Matrix matrix_b(2, 2);
+
+    matrix_a(0, 0) = 3.14;
+    matrix_a(0, 1) = 0.56;
+
+    matrix_b(0, 0) = -78.14;
+    matrix_b(0, 1) = 0;
+    matrix_b(1, 0) = -0.3;
+    matrix_b(1, 1) = 2;
+
+    EXPECT_THROW(matrix_a.SumMatrix(matrix_b), std::out_of_range);
+}
+TEST(SubMatrix, True) {
+    S21Matrix matrix_a(2, 2);
+    S21Matrix matrix_b(2, 2);
+    S21Matrix result(2, 2);
+
+    matrix_a(0, 0) = 3;
+    matrix_a(0, 1) = 2;
+    matrix_a(1, 0) = -6;
+    matrix_a(1, 1) = 0;
+
+    matrix_b(0, 0) = -7;
+    matrix_b(0, 1) = 0;
+    matrix_b(1, 0) = -3;
+    matrix_b(1, 1) = 2;
+
+    result(0, 0) = 10;
+    result(0, 1) = 2;
+    result(1, 0) = -3;
+    result(1, 1) = -2;
+
+    matrix_a.SubMatrix(matrix_b);
+
+    ASSERT_TRUE(matrix_a == result);
+}
+TEST(SubMatrix, False) {
+    S21Matrix matrix_a(1, 2);
+    S21Matrix matrix_b(2, 2);
+
+    matrix_a(0, 0) = 3;
+    matrix_a(0, 1) = 2;
+    matrix_b(0, 0) = -7;
+    matrix_b(0, 1) = 0;
+    matrix_b(1, 0) = -3;
+    matrix_b(1, 1) = 2;
+
+    EXPECT_THROW(matrix_a.SubMatrix(matrix_b), std::out_of_range);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
