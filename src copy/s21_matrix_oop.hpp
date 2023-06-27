568500701dccb59150812c229751ef42cca577b3
root <root@Lenovo-PC6.localdomain>
2023-06-28 00:46:22 +0300

    I have to fix segfault coredump

--- src copy/s21_matrix_oop.hpp
+++ src copy/s21_matrix_oop.hpp
@@ -27,11 +27,16 @@ public:
   void MulMatrix(const S21Matrix &other);
   void MulNumber(const double num);
   S21Matrix Transpose();
+  S21Matrix CalcComplements();
+  double Determinant();
+  S21Matrix InverseMatrix();
 
   // Overloads Operators
+  friend S21Matrix operator*(const double num, const S21Matrix &other);
   S21Matrix operator+(const S21Matrix &other);
   S21Matrix operator-(const S21Matrix &other);
   S21Matrix operator*(const S21Matrix &other);
+  S21Matrix operator*(const double num);
 
   // Assignement operators
   bool operator==(const S21Matrix &other);
@@ -39,14 +44,15 @@ public:
   S21Matrix &operator-=(const S21Matrix &other);
   S21Matrix &operator=(const S21Matrix &other);
   S21Matrix &operator*=(const S21Matrix &other);
+  S21Matrix &operator*=(const double num);
 
   // getters and setters
   void set_rows(int rows);
   void set_cols(int cols);
   int get_rows();
   int get_cols();
+  S21Matrix MinorMatrix(const S21Matrix &other, int rows, int cols);
   // Other methods..
   double &operator()(int row, int col);
 };
-
 #endif // S21_MATRIX_OOP_HPP
